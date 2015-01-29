4a2f6884882d544cae1f2e85cde1abca9c084141
Lukasz <lukasz.sitnik@meelogic.com>
2015-01-29 12:54:38 +0100

    added second example

--- libs/main.js
+++ libs/main.js
@@ -1,10 +1,34 @@
 /**
  * Created by meep_sitl on 28.08.14.
  */
-define(["OpeningHours", "jquery"], function (OpeningHours, $) {
+define(["OpeningHours", "sunlight", "jquery", "sunlight-xml", "sunlight-javascript"], function (OpeningHours, Sunlight, $) {
     "use strict";
 
-    var openingHours = new OpeningHours("#opening-hours", {});
+    var options = {};
+
+    Sunlight.highlightAll();
+
+    new OpeningHours("#opening-hours", options);
+
+    options = {
+        title: "Opening hours",
+        closedMsg: "Closed",
+        days: [
+            {name: "Su", hours: null},
+            {name: "Mo", hours: "09:00 - 19:00"},
+            {name: "Tu", hours: "10:00 - 20:00"},
+            {name: "We", hours: "11:00 - 21:00"},
+            {name: "Th", hours: "10:30 - 18:30"},
+            {name: "Fr", hours: "08:00 - 15:30"},
+            {name: "Sa", hours: null}
+        ],
+        cellWidth: "35px",
+        cellHeight: "30px",
+        selectedDay: 1,
+        firstDaySunday: true,
+        cssStyle: ""
+    };
+    new OpeningHours("#opening-hours2", options);
 
     console.log("OpeningHours loaded (AMD)");
 });
--- require-main.js
+++ require-main.js
@@ -3,7 +3,21 @@ requirejs.config({
     paths: {
         OpeningHours: 'libs/opening-hours.min',
         main: 'libs/main',
-        jquery: 'libs/jquery.min'
+        jquery: 'libs/jquery.min',
+        sunlight: 'libs/sunlight-min',
+        "sunlight-javascript": 'libs/sunlight.javascript-min',
+        "sunlight-xml": "libs/sunlight.xml-min"
+    },
+    shim: {
+        sunlight: {
+            exports: "Sunlight"
+        },
+        "sunlight-javascript": {
+            deps: ['sunlight']
+        },
+        "sunlight-xml": {
+            deps: ['sunlight']
+        }
     }
 });
 
